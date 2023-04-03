2e701a2fdb9dd126b647e9c2f28a12845f958091
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2023-04-03 13:30:41 +0900

    ヘッター　HTML/CSS完了

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,20 @@
 //アロー関数はrface
-import React from 'react'
+import React from 'react';
+import "./Home.css"
+import Rightbar from '../../components/rightbar/Rightbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Timeline from '../../components/timeline/Timeline'
 import Topbar from '../../components/topbar/Topbar'
 
 const Home = () => {
   return (
     <>
     <Topbar />
-    {/*sidebar*/}
-    {/*timeline*/}
-    {/*righbar*/}
+    <div className="homeContainer">
+    <Sidebar />
+    {/* <Timeline />
+    <Rightbar /> */}
+    </div>
     </>
   )
 }
