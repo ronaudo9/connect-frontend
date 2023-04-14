48569ee29319ef5f5e89556d7838c7dce9f05107
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2023-04-14 16:17:56 +0900

    ユーザーの新規登録機能（フロント）　実装

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,28 +1,85 @@
-import React from 'react'
-import "./Register.css"
+import axios from "axios";
+import React, { FormEvent, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Register.css";
 
 const Register = () => {
+  const username = useRef<HTMLInputElement | null>(null);
+  const email = useRef<HTMLInputElement | null>(null);
+  const password = useRef<HTMLInputElement | null>(null);
+  const passwordConfirmation = useRef<HTMLInputElement | null>(null);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    //フォームが送信された時にページがリロードされるのを防ぐ
+    e.preventDefault();
+    //パスワードと確認用パスワードがあっているかを確認
+    if (password.current?.value !== passwordConfirmation.current?.value) {
+      passwordConfirmation.current?.setCustomValidity("パスワードが違います");
+    } else {
+      try {
+        //registerAPIを叩く
+        const user = {
+          username: username.current?.value,
+          email: email.current?.value,
+          password: password.current?.value,
+        };
+        await axios.post("/auth/register", user);
+        navigate("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
   return (
     <div className="login">
-    <div className="loginWrapper">
-      <div className="loginLeft">
-        <h3 className="loginLogo">Connect</h3>
-        <span className="loginDesc">本格的なSNSを、自分の手で。</span>
-      </div>
-        <div className="loginRight">
-          <div className="loginBox">
-            <p className="loginMsg">新規登録はこちら</p>
-            <input type="text" className="loginInput" placeholder='ユーザー名'/>
-            <input type="text" className="loginInput" placeholder='Eメール'/>
-            <input type="text" className="loginInput" placeholder='パスワード'/>
-            <input type="text" className="loginInput" placeholder='確認用パスワード'/>
-            <button className = "loginButton">サインアップ</button>
-            <button className="loginRegisterButton">ログイン</button>
-          </div>
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Connect</h3>
+          <span className="loginDesc">本格的なSNSを、自分の手で。</span>
         </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={(e) => handleSubmit(e)}>
+            <p className="loginMsg">新規登録はこちら</p>
+            <input
+              type="text"
+              className="loginInput"
+              placeholder="ユーザー名"
+              required
+              ref={username}
+            />
+            <input
+              type="email"
+              className="loginInput"
+              placeholder="Eメール"
+              required
+              ref={email}
+            />
+            <input
+              type="password"
+              className="loginInput"
+              placeholder="パスワード"
+              required
+              minLength={6}
+              ref={password}
+            />
+            <input
+              type="password"
+              className="loginInput"
+              placeholder="確認用パスワード"
+              required
+              minLength={6}
+              ref={passwordConfirmation}
+            />
+            <button className="loginButton" type="submit">
+              サインアップ
+            </button>
+            <button className="loginRegisterButton">ログイン</button>
+          </form>
+        </div>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
